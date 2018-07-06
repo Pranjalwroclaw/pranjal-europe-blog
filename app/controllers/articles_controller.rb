9d0c730c76af95c510d155eb1a972982e69f4776
Pranjal Bharali <pranjalbharali@iclousd.com>
2018-07-06 13:13:46 +0000

    add partials and destroy metthos

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles =Article.all
+  end
+  
   def new
     @article=Article.new
   end
@@ -18,18 +22,26 @@ class ArticlesController < ApplicationController
     @article =Article.find(params[:id])
   end
   
+  def destroy
+    @article=Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "successfully deleted"
+    redirect_to articles_path
+  end
+  
   def edit
     @article =Article.find(params[:id])
   end
-    def update
-     @article =Article.find(params[:id])
+  
+  def update
+    @article =Article.find(params[:id])
      if @article.update(article_params)
-       flash[:notice]="article successfully updated"
-       redirect_to article_path(@article)
-      else
-        render :edit
-      end
-    end
+      flash[:notice]="article successfully updated"
+      redirect_to article_path(@article)
+     else
+      render :edit
+     end
+  end
     
   private
     def article_params
