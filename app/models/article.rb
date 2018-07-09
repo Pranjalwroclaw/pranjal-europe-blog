34883d518d82544f8b58194a0840a32bd5801948
Pranjal Bharali <pranjalbharali@iclousd.com>
2018-07-09 11:54:35 +0000

    change in length

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
   validates :title, presence: true, length: { minimum: 4, maximum: 30 }
-  validates :description, presence: true, length: { minimum: 10, maximum: 400 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 4000 }
 end
