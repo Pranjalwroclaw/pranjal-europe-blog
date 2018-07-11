b97e158e90bd9cc7b5ab25eab455658e613ed354
Pranjal Bharali <pranjalbharali@iclousd.com>
2018-07-11 12:59:41 +0000

    paginate user index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   before_action :set_articles, only:[:show, :destroy, :edit, :update]
   
   def index
-    @articles =Article.all
+   @articles = Article.paginate(page: params[:page], per_page: 3)
   end
   
   def new
