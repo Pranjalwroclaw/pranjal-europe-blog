ecdef8676a7f1c3a68ee0294170910016ba0377d
Pranjal Bharali <pranjalbharali@iclousd.com>
2018-07-13 11:44:28 +0000

    currect_user to be the creator or article

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "welcome to travel  blog #{@user.username}"
+      session[:user_id] = @user.id
       redirect_to articles_path
     else
       render 'new'
