07ec4e7a4477210918b335c3afd82709eed40df9
Pranjal Bharali <pranjalbharali@iclousd.com>
2018-07-13 12:34:22 +0000

    debugger removed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,9 +12,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] =@user.id
       flash[:success] = "welcome to travel  blog #{@user.username}"
-      redirect_to articles_path
-    else
+      redirect_to user_path(@user)
+    else 
       render 'new'
     end
   end
