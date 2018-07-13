42abcf383f8cdb14b26b3a91123de37f19c15ee8
Pranjal Bharali <pranjalbharali@iclousd.com>
2018-07-13 11:18:00 +0000

    currect_user to be the creator or article

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
+  before_action :require_same_user, only: [:edit, :update]
   def index
     @users = User.paginate(page: params[:page], per_page: 3)
   end
@@ -43,5 +44,12 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :email, :password)
   
+  end
+  
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = "u can only edit ur account"
+      redirect_to root_path
+    end
   end
 end
