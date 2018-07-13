70722e4ae7f3c6daf42ad768ed354166db4d88d1
Pranjal Bharali <pranjalbharali@iclousd.com>
2018-07-13 11:06:07 +0000

    restiction to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  
+  before_action :set_user, only: [:show, :edit, :update]
   def index
     @users = User.paginate(page: params[:page], per_page: 3)
   end
@@ -17,11 +17,11 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
   def edit
-    @user = User.find(params[:id])
   end
+  
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "updated successfully #{@user.username}"
       redirect_to articles_path
@@ -29,12 +29,17 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 3)
   end
   
   private 
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
   def user_params
     params.require(:user).permit(:username, :email, :password)
   
